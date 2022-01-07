eace7914806d2249845a9bca04a44a550ad61009
tyler komoroske <tjkomor@clemson.edu>
2022-01-07 10:01:46 -0700

    add impl block for snake

--- src/snake.rs
+++ src/snake.rs
@@ -7,3 +7,31 @@ pub struct Snake {
     direction: Direction,
     digesting: bool,
 }
+
+impl Snake {
+    pub fn new(start: Point, length: u16, direction: Direction) -> Self {
+        let opposite = direction.opposite();
+        let body: Vec<Point> = (0..length)
+            .into_iter()
+            .map(|i| start.transform(opposite, i))
+            .collect();
+
+        Self { body, direction, digesting: false }
+    }
+
+    pub fn get_head_point(&self) -> Point {
+        self.body.first().unwrap().clone()
+    }
+
+    pub fn get_body_points(&self) -> Vec<Point> {
+        self.body.clone()
+    }
+
+    pub fn get_direction(&self) -> Direction {
+        self.direction.clone()
+    }
+
+    pub fn contains_point(&self, point: &Point) -> bool {
+        self.body.contains(point)
+    }
+}
